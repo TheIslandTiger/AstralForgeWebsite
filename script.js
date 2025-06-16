908ff0f10883a327e00a78c454ca79febf48f1c9
Abbygale Dunn <115143875+TheIslandTiger@users.noreply.github.com>
2025-06-16 16:53:49 +0100

    Update script.js

--- script.js
+++ script.js
@@ -11,35 +11,6 @@ function myFunction() {
   }
 }
 
-// Allowing the user to pause and play the home video.
-var homeVideoStatusL = "play";
-var homeVideoStatusS = "play";
-
-// Large screens
-function HomeVideoL() { 
-  if(homeVideoStatusL == "play") {
-    homeVideoStatusL = "pause";
-    document.getElementById("HBWHomeVideoL").pause();
-  }
-    
-  else if (homeVideoStatusL == "pause"){
-    homeVideoStatusL = "play";
-    document.getElementById("HBWHomeVideoL").play();
-  }
-} 
-// Small screens
-function HomeVideoS() { 
-  if(homeVideoStatusS == "play") {
-    homeVideoStatusS = "pause";
-    document.getElementById("HBWHomeVideoS").pause();
-  }
-    
-  else if (homeVideoStatusS == "pause"){
-    homeVideoStatusS = "play";
-    document.getElementById("HBWHomeVideoS").play();
-  }
-} 
-
 // Creating the quote slide show
 var slideshows = document.querySelectorAll('[data-component="slideshow"]');
 slideshows.forEach(initSlideShow);
@@ -70,35 +41,4 @@ function jobTeamFilter() {
 
 function jobLocationFilter() {  
   document.getElementById("jobLocationDropdown").classList.toggle("jobLocationShow");
-}
-
-// Forms 
-function validateForm() {
-  let formName = document.forms["myForm"]["fName"].value;
-  let formEmail = document.forms["myForm"]["fEmail"].value;
-  let formConEmail = document.forms["myForm"]["fConEmail"].value;
-  let formSurname = document.forms["myForm"]["fSurname"].value;
-  let formPhone = document.forms["myForm"]["fPhone"].value;
-  let formResume = document.forms["myForm"]["fResume"].value;
-  
-  if (formName == "") {
-    alert("First name must be filled out");
-    return false;
-  }  
-  else if (formEmail == "") {
-    alert("Email must be filled out");
-    return false;
-  }  
-  else if (formConEmail == "" || formConEmail != formEmail) {
-    alert("Confirm email must be filled out and match the original email");
-    return false;
-  }  
-  else if(formSurname == "") {
-    alert("Surname must be filled out");
-    return false;
-  } 
-  else if (formPhone == "" || formPhone.length != 11) {
-    alert("Phone number must be filled out with the format 0XXXXXXXXXX");
-    return false;
-  }
 }
