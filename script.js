6ffd87f36ccfc347c2801b9794aa852b65577da6
Abbygale Dunn <115143875+TheIslandTiger@users.noreply.github.com>
2026-01-06 17:41:07 +0000

    Update script.js

--- script.js
+++ script.js
@@ -61,7 +61,7 @@ function showSlides(n) {
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
   }
-  slidesEvent[slideIndex-1].style.display = "block";
+  slidesEvent[slideIndex-1].style.display = "flex";
   dots[slideIndex-1].className += " active";
 }
 
