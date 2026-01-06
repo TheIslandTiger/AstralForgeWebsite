36ccba33c492b38a2924d6dcf96838bc5d259ca7
Abbygale Dunn <115143875+TheIslandTiger@users.noreply.github.com>
2026-01-06 17:48:02 +0000

    Update script.js

--- script.js
+++ script.js
@@ -35,34 +35,36 @@ function initSlideShow(slideshow) {
 }
 
 
-// Create the events slide show
-let slideIndex = 1;
-showSlides(slideIndex);
-
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  let i;
-  let slidesEvent = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
-  if (n > slidesEvent.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slidesEvent.length}
-  for (i = 0; i < slidesEvent.length; i++) {
-    slidesEvent[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slidesEvent[slideIndex-1].style.display = "flex";
-  dots[slideIndex-1].className += " active";
+document.addEventListener("DOMContentLoaded", function () {
+	// Create the events slide show
+	let slideIndex = 1;
+	showSlides(slideIndex);
+	
+	// Next/previous controls
+	function plusSlides(n) {
+	  showSlides(slideIndex += n);
+	}
+	
+	// Thumbnail image controls
+	function currentSlide(n) {
+	  showSlides(slideIndex = n);
+	}
+	
+	function showSlides(n) {
+	  let i;
+	  let slidesEvent = document.getElementsByClassName("mySlides");
+	  let dots = document.getElementsByClassName("dot");
+	  if (n > slidesEvent.length) {slideIndex = 1}
+	  if (n < 1) {slideIndex = slidesEvent.length}
+	  for (i = 0; i < slidesEvent.length; i++) {
+	    slidesEvent[i].style.display = "none";
+	  }
+	  for (i = 0; i < dots.length; i++) {
+	    dots[i].className = dots[i].className.replace(" active", "");
+	  }
+	  slidesEvent[slideIndex-1].style.display = "flex";
+	  dots[slideIndex-1].className += " active";
+	}
 }
 
 
