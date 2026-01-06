2ec239e7c5c2fd4e95f40c128f6c6597817754d9
Abbygale Dunn <115143875+TheIslandTiger@users.noreply.github.com>
2026-01-06 14:30:23 +0000

    Update script.js

--- script.js
+++ script.js
@@ -11,6 +11,7 @@ function myFunction() {
   }
 }
 
+
 // Creating the quote slide show
 var slideshows = document.querySelectorAll('[data-component="slideshow"]');
 slideshows.forEach(initSlideShow);
@@ -32,6 +33,38 @@ function initSlideShow(slideshow) {
 
 	}, time);
 }
+
+
+// Create the events slide show
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
+
 
 // Jobs dropdowns
 // When the user clicks on the button, toggle between hiding and showing the dropdown content 
