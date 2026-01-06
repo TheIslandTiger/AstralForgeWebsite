747e7034a8789f62e0c716f0e487ff19a95edca4
Abbygale Dunn <115143875+TheIslandTiger@users.noreply.github.com>
2026-01-06 17:52:03 +0000

    Update script.js

--- script.js
+++ script.js
@@ -62,7 +62,7 @@ document.addEventListener("DOMContentLoaded", function () {
 	  for (i = 0; i < dots.length; i++) {
 	    dots[i].className = dots[i].className.replace(" active", "");
 	  }
-	  slidesEvent[slideIndex-1].style.display = "flex";
+	  slidesEvent[slideIndex-1].style.display = "block";
 	  dots[slideIndex-1].className += " active";
 	}
 }
