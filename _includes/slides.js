fd905ddc9b84f5ea0545c295d484dfb2c1c5275f
qPalee <leealabaster04@yahoo.com>
2026-05-13 15:52:44 +0100

    Mobile stuff

--- _includes/slides.js
+++ _includes/slides.js
@@ -65,23 +65,4 @@ document.addEventListener("DOMContentLoaded", () => {
 
     return result;
   }
-
-  // Hamburger menu
-  let hamburger = document.getElementById("hamburger");
-  let menu = document.getElementById("hamburger-menu");
-
-  hamburger.addEventListener("click", () => {
-    menu.classList.toggle("visible");
-    if(hamburger.innerHTML == "☓"){
-      hamburger.innerHTML = "&#9776";
-    }else{
-      hamburger.innerHTML = "&#9747";
-    }
-  });
-
-  menu.addEventListener("click", (event) => {
-    if (event.target == menu) {
-      menu.classList.toggle("visible");
-    }
-  });
 });
